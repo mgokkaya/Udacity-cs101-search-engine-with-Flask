efd3eabd722dca226cbdd9afe87dbefeb2306e47
Muhammed <muhammeddgokkaya@gmail.com>
2019-02-12 00:49:51 +0300

    first commit

--- src/app.py
+++ src/app.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from flask import Flask,render_template,request  
-import dinamik_se                                     
+import search_engine                                     
 app = Flask(__name__, template_folder='templates') 
 
 @app.route('/',methods=["GET","POST"])   # link 127.0.0.1:5000/ 
@@ -10,9 +10,9 @@ def search():
 	word =""
 	if request.method == "POST":  
 		word = request.form.get("search")  
-		index, graph = dinamik_se.crawl_web('http://www.hurriyet.com.tr/')  #crawl fonction index
-		ranks = dinamik_se.compute_ranks(graph) #compute url ranks
-		result = dinamik_se.keyword_search(index, ranks, str(word)) 
+		index, graph = search_engine.crawl_web('http://www.hurriyet.com.tr/')  #crawl fonction index
+		ranks = search_engine.compute_ranks(graph) #compute url ranks
+		result = search_engine.keyword_search(index, ranks, str(word)) 
 		if result :
 			pass
 		else:
